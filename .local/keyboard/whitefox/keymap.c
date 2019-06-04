baf61e145a22aff7eb61b992a865258110936af5
Matt Symonds <symonds@users.noreply.github.com>
2019-06-04 23:21:26 +0100

    Updated keymap

--- .local/keyboard/whitefox/keymap.c
+++ .local/keyboard/whitefox/keymap.c
@@ -1,6 +1,6 @@
 #include QMK_KEYBOARD_H
 
-const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer */
     [0] = LAYOUT( \
         KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_NO,   KC_BSPC, KC_HOME, \
