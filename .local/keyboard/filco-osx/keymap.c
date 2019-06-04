be44090912b6b41525a354b11a36e1479daa4a4e
Matt Symonds <symonds@users.noreply.github.com>
2019-06-04 17:17:08 +0100

    Fixed filename

--- .local/keyboard/filco-osx/keymap.c
+++ .local/keyboard/filco-osx/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,  \
     KC_CLCK, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                 \
     KC_LSFT, KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,             \
-    KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, KC_RGUI,   MO(1), KC_RCTL,      KC_LEFT, KC_DOWN, KC_RGHT \
+    KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             KC_RALT, KC_RGUI,   MO(1), KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT   \
   ),
     /* Layer 1: Function layer */
     [KM_MEDIA] = LAYOUT( \
